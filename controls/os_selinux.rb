a7009dff547e08c7bcbad5e5ad2cff7688b8679a
aditya5597 <adityachatterjee05@gmail.com>
2021-10-13 13:56:14 +0000

    Added Selinux test

---
+++ controls/os_selinux.rb
@@ -0,0 +1,11 @@
+title "Testing Selinux"
+
+control "os-02" do                        # A unique ID for this control
+  impact 0.5                                # The criticality, if this control fails.
+  title "Check Selinux status"             # A human-readable title
+  describe selinux do                  # The actual test
+    it { should be_installed }
+    it { should be_permissive }
+    it { should_not be_enabled }
+  end
+end
